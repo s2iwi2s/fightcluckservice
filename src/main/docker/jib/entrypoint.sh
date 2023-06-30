6baf8c061e6881ce66cefc2d625bdc26337a30b9
Winston Pidor <s2iwi2s@gmail.com>
2023-06-30 17:11:54 -0500

    first commit

--- src/main/docker/jib/entrypoint.sh
+++ src/main/docker/jib/entrypoint.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
-
-echo "The application will start in ${JHIPSTER_SLEEP}s..." && sleep ${JHIPSTER_SLEEP}
-exec java ${JAVA_OPTS} -noverify -XX:+AlwaysPreTouch -Djava.security.egd=file:/dev/./urandom -cp /app/resources/:/app/classes/:/app/libs/* "com.s2i.lms.LearnMngtSysApp"  "$@"
+#echo "eureka.client.service-url.defaultZone=${eureka.client.service-url.defaultZone}"
+echo "SPRING_PROFILES_ACTIVE=${SPRING_PROFILES_ACTIVE}"
+echo "SPRING_DATASOURCE_URL=${SPRING_DATASOURCE_URL}"
+echo "JAVA_OPTS=${JAVA_OPTS}"
+echo "APP_DELAY_STARTUP=${APP_DELAY_STARTUP}"
+echo "The application will start in ${APP_DELAY_STARTUP}s..." && sleep ${APP_DELAY_STARTUP}
+exec java ${JAVA_OPTS} -noverify -XX:+AlwaysPreTouch -Djava.security.egd=file:/dev/./urandom -cp /app/resources/:/app/classes/:/app/libs/* "com.stoi.fightcluckservice.FightcluckserviceApplication"  "$@"
--- src/main/java/com/stoi/fightcluckservice/FightcluckserviceApplication.java
+++ src/main/java/com/stoi/fightcluckservice/FightcluckserviceApplication.java
@@ -1,13 +1,68 @@
 package com.stoi.fightcluckservice;
 
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Optional;
 
 @SpringBootApplication
 public class FightcluckserviceApplication {
+    private static final Logger log = LoggerFactory.getLogger(FightcluckserviceApplication.class);
 
-	public static void main(String[] args) {
-		SpringApplication.run(FightcluckserviceApplication.class, args);
-	}
+    public static void main(String[] args) {
+//        SpringApplication.run(FightcluckserviceApplication.class, args);
 
+        SpringApplication app = new SpringApplication(FightcluckserviceApplication.class);
+        Environment env = app.run(args).getEnvironment();
+
+        startupLog(env);
+    }
+
+    private static void startupLog(Environment env) {
+        String protocol = Optional.ofNullable(env.getProperty("server.ssl.key-store")).map(key -> "https").orElse("http");
+        String serverPort = env.getProperty("server.port");
+        String contextPath = Optional
+                .ofNullable(env.getProperty("server.servlet.context-path"))
+                .filter(StringUtils::isNotBlank)
+                .orElse("/");
+        String hostAddress = "localhost";
+        try {
+            hostAddress = InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            log.warn("The host name could not be determined, using `localhost` as fallback");
+        }
+        String dsUrl = env.getProperty("spring.datasource.url");
+        String dsUsername = env.getProperty("spring.datasource.username");
+        String eurekaDefaultZone = env.getProperty("eureka.client.service-url.defaultZone");
+
+
+        log.info(
+                "\n----------------------------------------------------------\n\t" +
+                        "Application '{}' is running!:\n\t" +
+                        "Local: \t\t{}://localhost:{}{}\n\t" +
+                        "External: \t{}://{}:{}{}\n\t" +
+                        "DS Url: \t{}\n\t" +
+                        "DS Username: \t{}\n\t" +
+                        "Eureka: \t{}\n\t" +
+                        "Profile(s): \t{}\n----------------------------------------------------------",
+                env.getProperty("spring.application.name"),
+                protocol,
+                serverPort,
+                contextPath,
+                protocol,
+                hostAddress,
+                serverPort,
+                contextPath,
+                dsUrl,
+                dsUsername,
+                eurekaDefaultZone,
+                env.getActiveProfiles()
+        );
+    }
 }
