2c0cc174cb43fc9d3a96cbaaef9e5b2dc45ca886
Winston Pidor <s2iwi2s@gmail.com>
2023-06-30 16:57:12 -0500

    Initial commit

---
+++ src/main/java/com/stoi/fightcluckservice/FightcluckserviceApplication.java
@@ -0,0 +1,13 @@
+package com.stoi.fightcluckservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FightcluckserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FightcluckserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stoi/fightcluckservice/FightcluckserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stoi.fightcluckservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FightcluckserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
